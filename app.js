0e28d581412b8ba2385b8195876946ffeb36cd5d
Simon Kaegi <simon.kaegi@gmail.com>
2016-11-29 17:13:42 -0500

    Update app.js

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ var PORT = 80;
 
 var app = express();
 app.get('/', function (req, res) {
-  res.send('Welcome to Bluemix DevOps with Docker. Lets go use Pipeline Services');
+  res.send('Welcome to Bluemix DevOps with Docker. Lets go use the Continuous Delivery Service');
 });
 
 app.listen(PORT)
